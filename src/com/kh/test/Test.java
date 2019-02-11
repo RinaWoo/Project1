591a7e781763797a6defbb8a5f515f4fa99be949
user2 <user2@KH_C>
2019-02-11 19:32:11 +0900

    test메세지

--- src/com/kh/test/Test.java
+++ src/com/kh/test/Test.java
@@ -4,9 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
-		System.out.println("Hello World2");
-		System.out.println("Hello World3");
+		System.out.println("Hello1 ");
+		System.out.println("Hello 2");
+		System.out.println("Hello 3");
 	}
 
 }
